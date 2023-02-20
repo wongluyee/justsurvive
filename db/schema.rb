b3918570e4fd5e3eecb5c7efa79b8580188ad43f
Joseph Sandoval <joesando@gmail.com>
2023-02-20 18:22:36 +0900

    finished booking model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_085149) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_091510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "experience_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["experience_id"], name: "index_bookings_on_experience_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "location"
@@ -38,5 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_085149) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "experiences"
+  add_foreign_key "bookings", "users"
   add_foreign_key "experiences", "users"
 end
