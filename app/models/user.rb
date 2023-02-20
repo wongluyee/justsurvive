a1d0abce087f36c92f97d257581d443dbf931404
Luyee Wong <wongluyee1015@hotmail.com>
2023-02-20 17:22:04 +0900

    Added user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
