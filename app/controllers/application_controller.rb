10138fabee297e1648f345c02131a01ea1b3a4e0
Luyee Wong <wongluyee1015@hotmail.com>
2023-02-21 11:21:37 +0900

    Changed email, added navbar and login/signup pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_091510) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.bigint "user_id", null: false
     t.bigint "experience_id", null: false
     t.datetime "created_at", null: false
