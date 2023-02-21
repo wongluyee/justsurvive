5c671f21333a87058269b0b93fc27c3bfcddcdf3
Joseph Sandoval <joesando@gmail.com>
2023-02-21 11:39:24 +0900

    added title to experiences model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning the DB..'
+Experience.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  Experience.create(
+    title: Faker::Restaurant.name,
+    location: Faker::Address.street_address,
+    description: Restaurant::CATEGORY.sample
+    price:
+    category:
+  )
+end
+puts "... created #{Experience.count} restaurants."
