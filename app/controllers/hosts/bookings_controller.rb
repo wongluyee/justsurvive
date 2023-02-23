f347f7eaddd5c82069712763d0af81b38885110c
joshua ng <joshuang_443@hotmail.com>
2023-02-23 13:35:33 +0900

    made the name of the user who booked the experience shown on the card

--- app/controllers/hosts/bookings_controller.rb
+++ app/controllers/hosts/bookings_controller.rb
@@ -1,6 +1,7 @@
 class Hosts::BookingsController < ApplicationController
 
   def index
-    @bookings = policy_scope([:hosts, Booking])
+    @bookings = policy_scope([:hosts, Booking]).includes(:user)
+
   end
 end
